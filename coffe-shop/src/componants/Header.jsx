dd9e23aa5e558073ee44f99de1ca6609c7aa8903
chandkaurati <chandkaurati@outlook.com>
2024-07-22 06:04:01 +0530

    modified Header and Offer Cards

--- coffe-shop/src/componants/Header.jsx
+++ coffe-shop/src/componants/Header.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import heroimage from "../../public/cofee.jpg";
+import coffesplash from "../../public/coffesplash.png"
+import papercup from  "../../public/papercup.png"
 import Button from "./Button";
 function Header() {
   return (
-    <div className="w-full  lg:flex lg:justify-between">
+    <div className="w-full  lg:flex lg:justify-between lg:p-5">
+      {/* LEFT  */}
       <div className="left_section
        xl:flex xl:flex-col xl:justify-center
        xl:gap-3
@@ -14,11 +17,14 @@ function Header() {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor <br />
           obcaecati blanditiis inventore consequuntur fugiat repellendus?
         </p>
-        <Button title={"Shop now"} classes={"bg-black text-white w-40 p-2"}/>
+        <Button title={"Shop now"} classes={"bg-black text-white w-40 p-2 rounded-full"}/>
       </div>
-      <div className="image_Container w-1/2">
-        <img className="w-[35rem]" src={heroimage} alt="" />
+      {/*  right image container */}
+      <div className="image_Container  flex flex-col justify-center p-0 items-center w-1/2">
+        <img className="w-[15rem]" src={coffesplash} alt="" /> 
+        <img className="w-[21rem]" src={papercup} alt="" /> 
       </div>
+
     </div>
   );
 }
--- coffe-shop/src/pages/Home.jsx
+++ coffe-shop/src/pages/Home.jsx
@@ -3,6 +3,10 @@ import Header from '../componants/Header'
 import Navbar from '../componants/Navbar'
 import Offercard from '../componants/OfferCard'
 import coffee from '../../public/cup.png'
+import cup from '../../public/cup2.png'
+import beans from '../../public/coffeBeans.png'
+import Pastry from  '../../public/Pastry.png'
+import Papercup  from  '../../public/papercup.png'
 function Home() {
   return (
     <div className='px-6'>
@@ -11,8 +15,8 @@ function Home() {
       <Header/>
      </header>
 
-     <div className='offers  lg:py-24'>
-      <span className='text-center border'>
+     <div className='offers  lg:py-24 flex flex-col gap-6'>
+      <span className='text-center borer flex flex-col  gap-6'>
         <h1 className='text-5xl font-semibold'>OUR DELICIOUS OFFER</h1>
         <p>Lorem ipsum dolor sit amet consectetur
          adipisicing elit. Amet reprehenderit sint,
@@ -20,11 +24,14 @@ function Home() {
       </span>
 
       <div className='offer_card lg:py-10 lg:flex lg:justify-between'>
-      <Offercard image={coffee} name={"TYPES OF COFFE"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
-      <Offercard image={coffee} name={"TYPES OF COFFE"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
-      <Offercard image={coffee} name={"TYPES OF COFFE"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
+      <Offercard image={cup} name={"TYPES OF COFFE"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
+      <Offercard image={beans} name={"BEAN VARIETIES"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
+      <Offercard image={Pastry} name={"COFFE & PASTRY"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
+      <Offercard image={Papercup} name={"COFFE TO GO"} text={"it amet consectetur adipisicing elit. Amet reprehenderit sint"}/>
       </div>
+
      </div>
+
     </div>
   )
 }
